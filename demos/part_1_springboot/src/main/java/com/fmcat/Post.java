76703264bc86ce4fc246789e497de11a4810c9c5
Francesc Sans <air.fsans@gmail.com>
2018-11-18 11:57:51 +0100

    Added Springboot simple demo

---
+++ demos/part_1_springboot/src/main/java/com/fmcat/Post.java
@@ -0,0 +1,87 @@
+package com.fmcat;
+
+
+import org.hibernate.annotations.Generated;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name="posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    @Size(max = 100)
+    @Column(unique = true)
+    private String title;
+
+    @NotNull
+    @Size(max = 250)
+    private String description;
+
+    @NotNull
+    @Lob
+    private String content;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Post post = (Post) o;
+
+        if (id != post.id) return false;
+        if (title != null ? !title.equals(post.title) : post.title != null) return false;
+        if (description != null ? !description.equals(post.description) : post.description != null) return false;
+        if (content != null ? !content.equals(post.content) : post.content != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (content != null ? content.hashCode() : 0);
+        return result;
+    }
+}
